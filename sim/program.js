00ef4b994853b5e6684c7b7b521d6fe8ae48f2d5
twiaight <83948122+twiaight@users.noreply.github.com>
2021-07-25 07:54:21 +0900

    Update program.js

--- sim/program.js
+++ sim/program.js
@@ -5,15 +5,86 @@ const b1 = document.querySelector("#graph1b");
 const ctx_c1 = c1.getContext('2d');
 const ctx_b1 = b1.getContext('2d');
 
+var time = 0;
 var count = 0;
 
-function main() {
-  count++;
-  if( count >= 60 ) {
-    document.write("0");
+var EPS = 0.0001; // 許容誤差
+var result1 = [];
+var result2 = [];
+
+function func_y(x) {
+    return Math.pow(x, 3.0) + x -  1.0;
+}
+
+function func_z(x) {
+    return 3.0 * Math.pow(x, 2.0) + 1.0;
+}
+
+function nibun(a, b) {
+    let c;
+
+    do {
+      c = (a + b) / 2.0; // 2分計算
+      document.write(c + "<br>");
+      result2.push(c);
+      if (func_y(c) * func_y(a) < 0) b = c; // 式(1.2)
+      else a = c; // 式(1.3)
+
+      count++;
+    } while (Math.abs(a - b) > EPS); // 収束判別　式(1.4)の変形
+    return c;
+}
+
+Bisection() {
+    let a = 0.0, b = 1.0; // 初期値
+
     count = 0;
+    console.log("x^3 + x - 1 の2分法による数値計算");
+    console.log("初期値 a=" + a);
+    console.log("初期値 b=" + b);
+    let x = nibun(a, b); // 解
+    document.write("近似解 x = " + x + "<br>");
+    document.write("計算回数:" + count + "<br>");
+}
+
+function Newton() {
+    console.log("x^3 + x - 1 のニュートン法による数値計算");
+
+    count = 0;
+    let a = 1.0;
+    let b;
+    console.log("初期値 a=" + a);
+
+    while (1) {
+      b = a - func_y(a) / func_z(a); // 式(1.9)
+      count++;
+      document.write(b + "<br>");
+      result1.push(b);
+      if (Math.abs(a - b) < EPS) break;  // 収束判定
+      else a = b;
+
+      if ( count > 100 ) break;
+    }
+    document.write("近似解 x = " + b + "<br>");
+    document.write("計算回数:" + count + "<br>");
+}
+
+function main() {
+  Newton();
+  Bisection();
+  
+  requestAnimationFrame(draw);
+}
+
+function draw() {
+  
+  
+  
+  time++;
+  if( time >= 60 ) {
+    // document.write("0");
+    time = 0;
   }
-  requestAnimationFrame(main);
 }
 
 function axis() {
@@ -26,7 +97,7 @@ function axis() {
 }
 
 window.addEventListener("load", axis);
-start.addEventLitener("click", main);
+start.addEventListener("click", main);
 
 
 /*
@@ -60,7 +131,7 @@ class UI {
    * ニュートン法による根の計算
    */
 /*
-  Newton() {
+  function Newton() {
     console.log("x^3 + x - 1 のニュートン法による数値計算");
 
     this.count = 0;
@@ -88,9 +159,9 @@ class UI {
    * @return {number} Y座標
    */
 /*
-  func_y(x) {
+ func_y(x) {
     return Math.pow(x, 3.0) + x -  1.0;
-  }
+  } 
 */
   /**
    * 根を求めたい関数の微分関数
