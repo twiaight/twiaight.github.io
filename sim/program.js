3f608c4d1ceda111ea99a5ac6489c52264649885
twiaight <83948122+twiaight@users.noreply.github.com>
2021-07-25 14:20:47 +0900

    Update program.js

--- sim/program.js
+++ sim/program.js
@@ -1,4 +1,9 @@
 const start = document.querySelector("#start");
+const clear = document.querySelector("#clear");
+const next = document.querySelector("#next");
+const button1 = document.querySelector("#b01");
+const button2 = document.querySelector("#b02");
+const button3 = document.querySelector("#b03");
 const c1 = document.querySelector("#graph1");
 const b1 = document.querySelector("#graph1b");
 const c2 = document.querySelector("#graph2");
@@ -12,7 +17,10 @@ const ctx_b1 = b1.getContext('2d');
 const ctx_c2 = c2.getContext('2d');
 const ctx_b2 = b2.getContext('2d');
 
-var time = 0;
+ctx_c1.strokeStyle = "rgb(0, 0, 255)";
+ctx_c2.strokeStyle = "rgb(0, 0, 255)";
+
+var sequence = 0;
 var count1 = 0;
 var count2 = 0;
 
@@ -28,6 +36,12 @@ var flag2 = 0;
 
 var text1;
 var text2;
+
+var stack = undefined;
+
+function eps() {
+  console.log(button.value);
+}
 
 function func_y(x) {
     return Math.pow(x, 3.0) + x -  1.0;
@@ -64,6 +78,7 @@ function Bisection() {
     console.log("x^3 + x - 1 の2分法による数値計算");
     console.log("初期値 a=" + a);
     console.log("初期値 b=" + b);
+    result2.push(b);
     let x = nibun(a, b); // 解
     // document.write("近似解 x = " + x + "<br>");
     // document.write("計算回数:" + count + "<br>");
@@ -72,10 +87,11 @@ function Bisection() {
 function Newton() {
     console.log("x^3 + x - 1 のニュートン法による数値計算");
 
-    count = 0;
+    count1 = 0;
     let a = 1.0;
     let b;
     console.log("初期値 a=" + a);
+    result1.push(a);
 
     while (1) {
       b = a - func_y(a) / func_z(a); // 式(1.9)
@@ -97,182 +113,125 @@ function Newton() {
 }
 
 function main() {
+  sub();
+  
   Newton();
   Bisection();
   
   requestAnimationFrame(draw);
 }
 
+function nextsequence() {
+  sequence = 1;
+}
+
 function draw() {
   
-  
-  
-  time++;
-  if( time >= 60 ) {
+  if( sequence == 1 ) {
+    
     if ( flag1 == 0 ) text1 = output1.pop()
-    if ( text1 != undefined ) comp1.innerHTML += text1 + "<br>";
-    else flag1 = 1;
+    if ( text1 != undefined ) {
+      comp1.innerHTML += text1 + "<br>";
+      draw1(text1);
+    } else flag1 = 1;
       
     if ( flag2 == 0 ) text2 = output2.pop()
-    if ( text2 != undefined ) comp2.innerHTML += text2 + "<br>";
-    else flag2 = 2;  
+    if ( text2 != undefined ) {
+      comp2.innerHTML += text2 + "<br>";
+      draw2(text2);
+    } else flag2 = 2;  
       
-    time = 0;
+    sequence = 0;
   }
-    
-  requestAnimationFrame(draw);
+  
+  if ( flag1 == 1 && flag2 == 1 ) cancelAnimationFrame();
+  else requestAnimationFrame(draw);
 }
 
 function axis() {
   ctx_b1.beginPath();
   ctx_b1.moveTo(0, b1.height/2);
   ctx_b1.lineTo(b1.width, b1.height/2);
-  ctx_b1.moveTo(b1.width/2,0);
-  ctx_b1.lineTo(b1.width/2, b1.height);
+  ctx_b1.moveTo(0, 0);
+  ctx_b1.lineTo(0, b1.height);
   ctx_b1.stroke();
     
   ctx_b2.beginPath();
   ctx_b2.moveTo(0, b2.height/2);
   ctx_b2.lineTo(b2.width, b2.height/2);
-  ctx_b2.moveTo(b2.width/2,0);
-  ctx_b2.lineTo(b2.width/2, b2.height);
+  ctx_b2.moveTo(0, 0);
+  ctx_b2.lineTo(0, b2.height);
   ctx_b2.stroke();
+  
+  func();
+}
+
+function func() {
+  let now  = -1.0;
+  let next = 0;
+  let x, y;
+  
+  for ( let i=1; i<460; i++ ) {
+    x = i/460;
+    y = Math.pow(x, 3.0) + x -  1.0;
+    next = y;
+    
+    ctx_b1.beginPath();
+    ctx_b1.moveTo((i-1), b1.height/2+now*230);
+    ctx_b1.lineTo(i, b1.height/2 +  next*230);
+    ctx_b1.stroke();
+    
+    ctx_b2.beginPath();
+    ctx_b2.moveTo((i-1), b2.height/2+now*230);
+    ctx_b2.lineTo(i, b2.height/2 +  next*230);
+    ctx_b2.stroke();
+    
+    now = next;
+  }
+}
+
+function draw1(x) {
+  if ( stack == undefined ) {
+    stack = x;
+  } else {
+    let y = Math.pow(stack, 3.0) + stack -  1.0;
+    
+    ctx_c1.beginPath();
+    ctx_c1.moveTo(stack*460,c1.height/2);
+    ctx_c1.lineTo(stack*460,c1.height/2 + y*230);
+    ctx_c1.lineTo(x*460, c1.height/2);
+    ctx_c1.stroke();
+    
+    stack = x;
+  }
+}
+
+function draw2(x) {
+  ctx_c2.beginPath();
+  ctx_c2.moveTo(x*460,0);
+  ctx_c2.lineTo(x*460, c2.height);
+  ctx_c2.stroke();
+}
+
+function sub() {
+  flag1 = 0;
+  flag2 = 0;
+  comp1.innerHTML = "";
+  comp2.innerHTML = "";
+  result1 = [];
+　result2 = [];
+　output1 = [];
+　output2 = [];
+　stack = undefined;
+　sequence = 0;
+　ctx_c1.clearRect(0, 0, c1.width, c1.height);
+　ctx_c2.clearRect(0, 0, c2.width, c2.height);
 }
 
 window.addEventListener("load", axis);
 start.addEventListener("click", main);
-
-
-/*
-class UI {
-
-  constructor(start, c1, b1) {
-    this.start = start;
-    
-    this.c1 = c1;
-    this.ctxc1 = this.c1.getContext("2d");
-    this.b1 = b1;
-    this.ctxb1 = this.b1.getContext("2d");
-
-    this.EPS = 0; // 許容誤差
-    this.count = 0;
-    this.result1 = [];
-    this.result2 = [];
-    
-    this.axis();  
-    
-    this.start.addEventListener("click", () => {
-      this.EPS = 0.0001;
-      console.log('\n' + "0.0001");
-      this.Newton();
-      this.Bisection();
-      this.Graph();
-    });
-  }
-*/
-  /**
-   * ニュートン法による根の計算
-   */
-/*
-  function Newton() {
-    console.log("x^3 + x - 1 のニュートン法による数値計算");
-
-    this.count = 0;
-    let a = 1.0;
-    let b;
-    console.log("初期値 a=" + a);
-
-    while (1) {
-      b = a - this.func_y(a) / this.func_z(a); // 式(1.9)
-      this.count++;
-      document.write(b + "<br>");
-      this.result1.push(b);
-      if (Math.abs(a - b) < this.EPS) break;  // 収束判定
-      else a = b;
-
-      if ( this.count > 100 ) break;
-    }
-    document.write("近似解 x = " + b + "<br>");
-    document.write("計算回数:" + this.count + "<br>");
-  }
-*/
-  /**
-   * 根を求めたい関数
-   * @param {number} x X座標
-   * @return {number} Y座標
-   */
-/*
- func_y(x) {
-    return Math.pow(x, 3.0) + x -  1.0;
-  } 
-*/
-  /**
-   * 根を求めたい関数の微分関数
-   * @param {number} x X座標
-   * @return {number} Y座標
-   */
-/*
-  func_z(x) {
-    return 3.0 * Math.pow(x, 2.0) + 1.0;
-  }
-*/
-  //////////////////////////
-
-  /**
-   * 2分法による根の計算
-   */
-/*
-  Bisection() {
-    let a = 0.0, b = 1.0; // 初期値
-
-    this.count = 0;
-    console.log("x^3 + x - 1 の2分法による数値計算");
-    console.log("初期値 a=" + a);
-    console.log("初期値 b=" + b);
-    let x = this.nibun(a, b); // 解
-    document.write("近似解 x = " + x + "<br>");
-    document.write("計算回数:" + count + "<br>");
-  }
-*/
-  /**
-   * 実際の計算部分
-   * @param {number} a 計算範囲
-   * @param {number} b 計算範囲
-   * @return {number} 近似解
-   */
-/*
-  nibun(a, b) {
-    let c;
-
-    do {
-      c = (a + b) / 2.0; // 2分計算
-      document.write(c + "<br>");
-      this.result2.push(c);
-      if (this.func_y(c) * this.func_y(a) < 0) b = c; // 式(1.2)
-      else a = c; // 式(1.3)
-
-      this.count++;
-    } while (Math.abs(a - b) > this.EPS); // 収束判別　式(1.4)の変形
-    return c;
-  }
-*/
-  /**
-   * 根を求めたい関数
-   * @param {number} x X座標
-   * @return {number} Y座標
-   */
-/*
-  func_y(x) {
-    return Math.pow(x, 3.0) + x - 1.0;
-  }
-  
-  axis() {
-    this.ctxb1.beginPath();
-    this.ctxb1.moveTo(0,this.b1.height/2);
-    this.ctxb1.lineTo(this.b1.width,this.b1.height/2);
-    this.ctxb1.moveTo(this.b1.width/2,0);
-    this.ctxb1.lineTo(this.b1.width/2,this.b1.height);
-    this.ctxb1.stroke();
-  }
-}
-*/
+clear.addEventListener("click", sub);
+next.addEventListener("click",  nextsequence);
+button1.addEventListener("click", () => { EPS = Number(button1.value) });
+button2.addEventListener("click", () => { EPS = Number(button2.value) });
+button3.addEventListener("click", () => { EPS = Number(button3.value) });
